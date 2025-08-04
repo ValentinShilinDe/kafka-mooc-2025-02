472cac2891e8ba3348a8db1b2637943a3c01ef02
valen <valentin.shilin.de@gmail.com>
2025-08-04 18:19:27 +0200

    zio, akka

--- src/main/zio/zioStreaming.scala
+++ src/main/zio/zioStreaming.scala
@@ -1,56 +1,60 @@
-package zio
+package ru.otus.module3.zio
 
-import zio.kafka.producer.{Producer, ProducerSettings}
-import zio.kafka.consumer.{Consumer, ConsumerSettings, Subscription}
-import zio.stream.ZStream
+import zio._
+import zio.kafka.consumer._
+import zio.kafka.producer._
 import zio.kafka.serde._
-import ch.qos.logback.classic.{Level,Logger}
+import zio.stream._
+import scala.util.Random
+import zio.Console
+import zio.durationInt    // ← вот нужный импорт
+
+import ch.qos.logback.classic.{Level, Logger}
 import org.slf4j.LoggerFactory
 
 object MainApp extends ZIOAppDefault {
-  LoggerFactory
-    .getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME)
-    .asInstanceOf[Logger]
-    .setLevel(Level.ERROR)
+  LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME).asInstanceOf[Logger].setLevel(Level.ERROR)
 
-  val producer =
+  val producerLayer:ZLayer[Any, Throwable, Producer] =
+    ZLayer.scoped{
+      Producer.make(ProducerSettings(List("localhost:29093")))
+    }
+
+  val consumerLayer:ZLayer[Any, Throwable,Consumer] =
+    ZLayer.scoped {
+      Consumer.make(
+        ConsumerSettings(List("localhost:29093"))
+          .withGroupId("group")
+          .withClientId("zio-client")
+      )
+    }
+
+  val producerStream:ZStream[Producer, Throwable, Nothing] =
     ZStream
-      .repeatZIO(Random.nextIntBetween(0, Int.MaxValue))
-      .schedule(Schedule.fixed(2.seconds))
+      .repeatZIO(ZIO.attempt(Random.nextInt(Int.MaxValue)))
+      .schedule(Schedule.spaced(1.second))
       .mapZIO{random =>
         Producer.produce[Any, Long, String](
           topic = "random",
-          key = random % 4,
-          value = random.toString,
+          key= random%4,
+          value=random.toString,
           keySerializer = Serde.long,
           valueSerializer = Serde.string
         )
       }.drain
 
-  val consumer =
-    Consumer
-      .plainStream(Subscription.topics("random"), Serde.long, Serde.string)
-      .tap(r => Console.printLine(r.value))
+  val consumerStream:ZStream[Consumer, Throwable, Nothing] =
+    Consumer.plainStream(Subscription.topics("random"),
+        Serde.long, Serde.string)
+      .tap(record => Console.printLine(s"Consumed: ${record.value}"))
       .map(_.offset)
       .aggregateAsync(Consumer.offsetBatches)
       .mapZIO(_.commit)
       .drain
 
-  def producerLayer = ZLayer.scoped(
-    Producer.make(settings = ProducerSettings(List("localhost:29093")))
-  )
-
-  def consumerLayer =
-    ZLayer.scoped(
-      Consumer.make(
-        ConsumerSettings(List("localhost:29093")).withGroupId("group")
-      )
-    )
-
-  override def run:ZIO[Any, Throwable, Unit] =
-    producer.merge(consumer)
+  override def run: ZIO[Any, Throwable, Unit] =
+    producerStream
+      .merge(consumerStream)
       .runDrain
       .provide(producerLayer, consumerLayer)
-
-
 }
